9e99e1dc0ab8df319557acd5e5b6024a36c1bef7
IsmailHusun19 <ismailhusun2002>
2026-07-17 19:05:41 +0700

    main

--- src/app.js
+++ src/app.js
@@ -18,6 +18,13 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
+app.get("/", (req, res) => {
+  res.send("API OK");
+});
+
+app.get("/test", (req, res) => {
+  res.json({ ok: true });
+});
 
 app.use("/users", userRoute);
 app.use("/bahan", Bahan);
